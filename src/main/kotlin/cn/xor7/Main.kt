b3b16eb07cce3479e918b1b58244b15126459439
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-12-26 21:59:53 +0800

    feat: add timer

--- src/main/kotlin/cn/xor7/Main.kt
+++ src/main/kotlin/cn/xor7/Main.kt
@@ -7,12 +7,19 @@ import io.ktor.server.netty.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.util.*
+import kotlin.concurrent.scheduleAtFixedRate
 
 @Volatile
 private var cacheData: String = "[]"
 
 fun main() {
     println("starting cache server...")
+
+    Timer("request", true).scheduleAtFixedRate(5000, 0) {
+        cacheData = "[]"
+    }
+
     embeddedServer(Netty, 8080) {
         setupCacheServer()
     }.start(wait = true)

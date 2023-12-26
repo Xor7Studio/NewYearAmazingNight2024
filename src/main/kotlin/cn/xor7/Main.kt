c0bc864bef96164df9dabfa0102feda24f5f349d
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-12-26 17:26:20 +0800

    feat: add cacheServer entrypoint

--- src/main/kotlin/cn/xor7/Main.kt
+++ src/main/kotlin/cn/xor7/Main.kt
@@ -1,5 +1,33 @@
 package cn.xor7
 
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
 fun main() {
     println("starting cache server...")
+    embeddedServer(Netty, 8080) {
+        setupCacheServer()
+    }.start(wait = true)
+}
+
+private fun Application.setupCacheServer() {
+    install(CORS) {
+        allowHeader(HttpHeaders.AccessControlAllowOrigin)
+        allowHeader(HttpHeaders.ContentType)
+        allowNonSimpleContentTypes = true
+        allowCredentials = true
+        allowSameOrigin = true
+        anyHost()
+    }
+
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
+        }
+    }
 }

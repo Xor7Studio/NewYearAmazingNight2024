6e29599cb0567b6980f05b7c56ebdda77fdc0c47
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-12-29 19:25:37 +0800

    refactor: use commandAPI

--- src/main/kotlin/cn/xor7/Command.kt
+++ src/main/kotlin/cn/xor7/Command.kt
@@ -1,5 +1,7 @@
 package cn.xor7
 
+import cn.xor7.map.GameMap
+import dev.jorel.commandapi.kotlindsl.anyExecutor
 import dev.jorel.commandapi.kotlindsl.commandTree
 import dev.jorel.commandapi.kotlindsl.playerArgument
 import dev.jorel.commandapi.kotlindsl.playerExecutor
@@ -11,6 +13,22 @@ object Command {
             playerArgument("player", optional = true) {
                 playerExecutor { sender, args ->
                     (args.getOptional("player").orElse(sender) as Player).toggleDevelopmentMode()
+                }
+            }
+        }
+        commandTree("p") {
+            anyExecutor { _, _ ->
+                GameMap.toggleMapParticle()
+                GameMap.toggleRadiusParticle()
+            }
+            commandTree("map") {
+                anyExecutor { _, _ ->
+                    GameMap.toggleMapParticle()
+                }
+            }
+            commandTree("radius") {
+                anyExecutor { _, _ ->
+                    GameMap.toggleRadiusParticle()
                 }
             }
         }
